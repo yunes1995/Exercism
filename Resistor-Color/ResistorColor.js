ca79acca7616f6abc202a88ab40f07169f7bc652
yunes_mohammadi <88889070+yunes1995@users.noreply.github.com>
2023-02-09 14:10:24 +0330

    Create ResistorColor.js

---
+++ Resistor-Color/ResistorColor.js
@@ -0,0 +1,14 @@
+
+export const colorCode = (Cc) => { return COLORS.indexOf(Cc); };
+export const COLORS = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+  ];
